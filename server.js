7f3b32698593d569cbf0e55081fe19d06189ddf9
borys <borys.ponomarenko@adyen.com>
2018-06-25 10:26:55 +0200

    work on middleware

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const path = require('path');
 const express = require('express');
 const createMiddleware = require('./index.js');
 
-// const app = express();
+const app = express();
 
 createMiddleware(path.resolve('./petstore.yaml'))
   .then(middleware => {
-    console.log(middleware);
-    // app.use(middleware);
-    // app.listen(8080);
+    // console.log(middleware);
+    app.use(middleware);
+    app.listen(8090);
   });
